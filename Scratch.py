39d04fe2a26c1411a25b7e268858d1a950c96e21
Armand de Pompignan <arm2pom@mail.com>
2024-11-07 09:09:01 +0100

    début du code

--- Scratch.py
+++ Scratch.py
@@ -14,4 +14,22 @@ Armand de Pompignan
 Objectif création d'un jeu space invader
 """
 
-import tkinter as tk 
+import tkinter as tk
+
+# Créer la fenêtre principale
+fenetre = tk.Tk()
+fenetre.title("Exemple de Canvas Tkinter")
+
+# Définir les dimensions de la fenêtre
+fenetre.geometry("400x400")
+
+# Créer un canvas
+canvas = tk.Canvas(fenetre, width=300, height=300, bg="lightblue")
+canvas.pack(pady=20)
+
+bouton_quitter = tk.Button(fenetre, text="Quitter", command=fenetre.quit, bg ="red", fg="white")
+bouton_quitter.place(x=350, y=10)
+
+
+# Lancer la boucle principale
+fenetre.mainloop()
